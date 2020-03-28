768cdc6b744f4e29b9a974891d37520f86d439bb
Kevin Mathias <komathias1@gmail.com>
2020-03-28 04:36:12 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -22,11 +22,15 @@ end
 
 def take_a_number(katz_deli, name)
   if katz_deli == []
+<<<<<<< HEAD
     katz_deli << name
+=======
+>>>>>>> 453862354e68c05b232ac85edc341fe715883b2f
     puts "Welcome, #{name}. You are number 1 in line."
   else
   
  katz_deli << name
+<<<<<<< HEAD
  #katz_deli.each.with_index(1) do |names, i|
   puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
 end
@@ -41,3 +45,12 @@ def now_serving(katz_deli)
     katz_deli.shift
   end
 end
+=======
+ katz_deli.each.with_index(1) do |names, i|
+ 
+ end
+   
+puts "Welcome, #{katz_deli}. You are number #{i} in line."
+end
+end
+>>>>>>> 453862354e68c05b232ac85edc341fe715883b2f
